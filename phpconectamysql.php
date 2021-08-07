695cba9aa57c63b4fb3c6741560e5b290894d71b
Arthurjm1 <47225137+Arthurjm1@users.noreply.github.com>
2021-08-06 23:53:49 -0300

    Update phpconectamysql.php

--- phpconectamysql.php
+++ phpconectamysql.php
@@ -10,15 +10,15 @@ $banco = 'phalcont_teste01';
 // conectando com MYSQLI
 
 
-$mysqli = new mysqli($servidor, $usuario, $senha, $banco);
-if (mysqli_connect_errno()) trigger_error(mysqli_connect_error());
+// $mysqli = new mysqli($servidor, $usuario, $senha, $banco);
+// if (mysqli_connect_errno()) trigger_error(mysqli_connect_error());
 
-$sql = "SELECT * FROM imobiliaria";
-$result = $mysqli -> query($sql);
-$pai = mysqli_fetch_array($result);
+// $sql = "SELECT * FROM imobiliaria";
+// $result = $mysqli -> query($sql);
+// $pai = mysqli_fetch_array($result);
 
-var_dump($pai);
-echo "<BR><br>";
+// var_dump($pai);
+// echo "<BR><br>";
 
 
 // conectando com PDO 
@@ -26,9 +26,9 @@ echo "<BR><br>";
 $con = new PDO("mysql:host=".$servidor.";dbname=".$banco, $usuario, $senha);
     
    
-$rs = $con->prepare($sql);
-$rs->execute();
-$rows = $rs->fetchAll();
-var_dump($rows);
+// $rs = $con->prepare($sql);
+// $rs->execute();
+// $rows = $rs->fetchAll();
+// var_dump($rows);
 
 		?>
